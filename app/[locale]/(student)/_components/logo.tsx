a3ceaf8306719a4cff8216989f64f3293eb08dd8
GabrielSoo <gabrielswk2001@gmail.com>
2025-03-19 00:38:20 +0800

    updated dashboard for testing

--- app/[locale]/(student)/_components/logo.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-const Logo = () => {
-  return <Image height={90} width={90} alt="logo" src="/cube_logo.png" />;
-};
-
-export default Logo;
--- app/[locale]/(student)/dashboard/page.tsx
+++ app/[locale]/(student)/dashboard/page.tsx
@@ -1,15 +1,6 @@
 import { redirect } from "@/i18n/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { getLocale } from "next-intl/server";
-import { MyCourses } from "../../components/coursesPage/MyCourses";
-import Sidebar from "../_components/sidebar";
-import {
-  Sheet,
-  SheetContent,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
 
 export default async function Home() {
   const supabase = await createClient();
@@ -21,35 +12,7 @@ export default async function Home() {
     redirect({ href: "/login", locale });
   }
 
-  const userId = data.user!.id;
-
   return (
-    <section className="h-screen flex">
-      {/* Sidebar (Desktop) */}
-      <div className="hidden md:flex h-full w-60">
-        <Sidebar />
-      </div>
-
-      {/* Main Content Area */}
-      <div className="w-full flex-1 flex flex-col justify-center">
-        <div className="flex items-center md:h-[80px] border-b border-primary">
-          {/* Mobile Layout */}
-          <Sheet>
-            <SheetTrigger className="hover:opacity-75 transition md:hidden flex justify-start py-4 pl-7">
-              <Menu />
-            </SheetTrigger>
-            <SheetTitle className="hidden"></SheetTitle>
-            <SheetContent side="left" className="p-0 w-60">
-              <Sidebar />
-            </SheetContent>
-          </Sheet>
-        </div>
-
-        {/* Children (Page Content) */}
-        <main className="flex-1 overflow-y-auto mx-auto w-full min-w-0 px-8">
-          <MyCourses userId={userId} basePath="/dashboard" />
-        </main>
-      </div>
-    </section>
+    <div>hi</div>>
   );
 }
